7e6ff368fd397672a32a20914d1a674612efbe53
libw <3180234769@qq.com>
2018-07-18 09:37:18 +0800

    1

--- src/containers/operationLog/index.js
+++ src/containers/operationLog/index.js
@@ -1,7 +1,25 @@
 import React from 'react'
 import style from './index.css'
-import {hashHistory} from 'react-router'
-import {Layout, Menu, Breadcrumb, Icon, Button, Table, Dropdown, notification, Steps, Input,Select,Form,Badge,DatePicker} from 'antd';
+import {hashHistory,Link} from 'react-router'
+import {
+    Layout,
+    Menu,
+    Breadcrumb,
+    Icon,
+    Button,
+    Table,
+    Dropdown,
+    notification,
+    Steps,
+    Input,
+    Select,
+    Form,
+    Badge,
+    DatePicker
+} from 'antd';
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
+import {getLogList} from "../../actions/log";
 
 const Option = Select.Option;
 const {SubMenu} = Menu;
@@ -10,20 +28,20 @@ const Step = Steps.Step;
 const FormItem = Form.Item;
 const RangePicker = DatePicker.RangePicker;
 
-const data=[
+const data = [
     {
         key: 1,
         time: '2018-6-1 00:30:12',
         log: '创建基金-基金0001号',
         account: 'zhangsan@p95.com',
         name: '张三',
-    },{
+    }, {
         key: 2,
         time: '2018-6-1 00:30:12',
         log: '创建基金-基金0001号',
         account: 'zhangsan@p95.com',
         name: '张三',
-    },{
+    }, {
         key: 3,
         time: '2018-6-1 00:30:12',
         log: '创建基金-基金0001号',
@@ -36,13 +54,13 @@ const data=[
         log: '创建基金-基金0001号',
         account: 'zhangsan@p95.com',
         name: '张三',
-    },{
+    }, {
         key: 5,
         time: '2018-6-1 00:30:12',
         log: '创建基金-基金0001号',
         account: 'zhangsan@p95.com',
         name: '张三',
-    },{
+    }, {
         key: 6,
         time: '2018-6-1 00:30:12',
         log: '创建基金-基金0001号',
@@ -55,19 +73,19 @@ const data=[
         log: '创建基金-基金0001号',
         account: 'zhangsan@p95.com',
         name: '张三',
-    },{
+    }, {
         key: 8,
         time: '2018-6-1 00:30:12',
         log: '创建基金-基金0001号',
         account: 'zhangsan@p95.com',
         name: '张三',
-    },{
+    }, {
         key: 9,
         time: '2018-6-1 00:30:12',
         log: '创建基金-基金0001号',
         account: 'zhangsan@p95.com',
         name: '张三',
-    },{
+    }, {
         key: 10,
         time: '2018-6-1 00:30:12',
         log: '创建基金-基金0001号',
@@ -79,8 +97,7 @@ const data=[
 class Home extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-        };
+        this.state = {};
     }
 
     handleChange(value) {
@@ -95,26 +112,47 @@ class Home extends React.Component {
         e.preventDefault();
         this.props.form.validateFields((err, values) => {
             if (!err) {
-                console.log('Received values of form: ', values);
+                this.props.getLogList({
+                    page: 1,
+                    beginTime: this.state.beginTime,
+                    endTime: this.state.endTime,
+                    account: this.state.account,
+                    adminName: this.state.adminName
+                }, () => {
+
+                })
             }
         });
     }
 
+    
+    componentDidMount() {
+        this.props.getLogList({
+            page: 1
+        }, () => {
+
+        })
+    }
+
     render() {
-        const { getFieldDecorator } = this.props.form;
+        if (!this.props.log.logList) {
+            return null
+        }
+        const {getFieldDecorator} = this.props.form;
 
         const columns = [
-            { title: '时间', dataIndex: 'time', key: 'time' },
-            { title: '操作日志', dataIndex: 'log', key: 'log' },
-            { title: '操作账号', dataIndex: 'account', key: 'account' },
-            { title: '姓名', dataIndex: 'name', key: 'name' },
-            { title: '操作',  key: 'action', render: (text, record) => (
-                    <a href="javascript:void (0)">查看</a>
+            {title: '时间', dataIndex: 'time', key: 'time'},
+            {title: '操作日志', dataIndex: 'content', key: 'log'},
+            {title: '操作账号', dataIndex: 'account', key: 'account'},
+            {title: '姓名', dataIndex: 'name', key: 'name'},
+            {
+                title: '操作', key: 'action', render: (text, record) => (
+                    <Link to={'/logDetails/'+record.id}>查看</Link>
                 ),
             },
         ];
         const rangeConfig = {
-            rules: [{ type: 'array', required: true, message: 'Please select time!' }],
+            rules: [{type: 'array', required: true, message: 'Please select time!'}],
         };
 
         return (
@@ -122,7 +160,7 @@ class Home extends React.Component {
                 <span className={style.title}>操作日志</span>
                 <Form onSubmit={this.handleSubmit.bind(this)} className="login-form">
                     <div className={style.contentT}>
-                        基金名称12983812984
+                        筛选日志列表
                     </div>
                     <div className={style.content}>
                         <div className={style.inputBox}>
@@ -130,8 +168,17 @@ class Home extends React.Component {
                                 <span className={style.inputBoxT}>
                                     日期
                                 </span>
-                                {getFieldDecorator('range-picker', {rules: [{ type: 'array', required: true, message: '请选择时间范围!' }],})(
-                                    <RangePicker />
+                                {getFieldDecorator('range-picker', {
+                                    rules: [{
+                                        type: 'array',
+                                        required: true,
+                                        message: '请选择时间范围!'
+                                    }],
+                                })(
+                                    <RangePicker onChange={(e) => {
+                                        this.setState({beginTime:new Date(e[0]._d).valueOf()})
+                                        this.setState({endTime:new Date(e[1]._d).valueOf()})
+                                    }}/>
                                 )}
                             </FormItem>
                         </div>
@@ -141,9 +188,11 @@ class Home extends React.Component {
                                     操作账号
                                 </span>
                                 {getFieldDecorator('account', {
-                                        rules: [{ required: true, message: '请填写你的角色名!' }],
-                                    })(
-                                        <Input size="large" placeholder="请输入你的角色名"/>)}
+                                    rules: [{required: true, message: '请填写你的角色名!'}],
+                                })(
+                                    <Input onChange={(e) => {
+                                        this.setState({account: e.target.value})
+                                    }} size="large" placeholder="请输入你的角色名"/>)}
                             </FormItem>
                         </div>
                         <div className={style.inputBox}>
@@ -152,9 +201,11 @@ class Home extends React.Component {
                                     姓名
                                 </span>
                                 {getFieldDecorator('name', {
-                                        rules: [{ required: true, message: '请填写你的角色名!' }],
-                                    })(
-                                        <Input size="large" placeholder="请输入你的角色名"/>)}
+                                    rules: [{required: true, message: '请填写你的角色名!'}],
+                                })(
+                                    <Input onChange={(e) => {
+                                        this.setState({adminName: e.target.value})
+                                    }} size="large" placeholder="请输入你的角色名"/>)}
                             </FormItem>
                         </div>
                         <div className={style.button}>
@@ -166,7 +217,7 @@ class Home extends React.Component {
                     <Table
                         className="components-table-demo-nested"
                         columns={columns}
-                        dataSource={data}
+                        dataSource={this.props.log.logList.list}
                     />
                 </div>
             </div>
@@ -176,5 +227,18 @@ class Home extends React.Component {
     }
 }
 
+function mapStateToProps(state, props) {
+    return {
+        log: state.log
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        getLogList: bindActionCreators(getLogList, dispatch)
+    }
+}
+
+Home = connect(mapStateToProps, mapDispatchToProps)(Home)
 const WrappedHome = Form.create()(Home);
 export default WrappedHome
