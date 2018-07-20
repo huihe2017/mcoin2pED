466c3f1e5c5812245d28a1a89bf93cb53ee8ccd1
Your Name <you@example.com>
2018-07-20 09:39:00 +0800

    11111111
    12222

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,11 @@
 var config = {
 
-    // api_url: 'https://cpadm.fund.coin2p.com/',
-    // noauth_url: 'https://cpadm.fund.coin2p.com/',
+    api_url: 'https://cpadm.fund.coin2p.com/',
+    noauth_url: 'https://cpadm.fund.coin2p.com/',
 
 
-    api_url: 'https://www.easy-mock.com/mock/5b455d390b192b12c0cc85e9/cpadm/',
-    noauth_url: 'https://www.easy-mock.com/mock/5b455d390b192b12c0cc85e9/cpadm/',
+    // api_url: 'https://www.easy-mock.com/mock/5b455d390b192b12c0cc85e9/cpadm/',
+    // noauth_url: 'https://www.easy-mock.com/mock/5b455d390b192b12c0cc85e9/cpadm/',
 
 }
 module.exports = config
